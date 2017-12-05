c391d9b4feccd7d0f49503a7cbb8a6fac7033954
Xein <xeinmail@gmai.com>
2017-12-05 15:41:33 -0600

    first working iteration of gigasecond exercise

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+long long totalSeconds(int years, int months, int days, int hours, int minutes, int seconds)
+{
+	return (long long)seconds + (long long)minutes * 60 + (long long)hours * 60 * 60 + (long long)days * 24 * 60 * 60 + (long long)months * 30 * 24 * 60 * 60 + (long long)years * 12 * 30 * 24 * 60 * 60;
+	
+
+
+}
+
+long long diferenceInSeconds(int currentYear, int currentMonth, int currentDay, int currentHour, int currentMinute, int currentSecond, int yearOfBirth, int monthOfBirth, int dayOfBirth, int hourOfBirth, int minuteOfBirth, int secondOfBirth)
+{
+	return totalSeconds(currentYear, currentMonth, currentDay, currentHour, currentMinute, currentSecond) - totalSeconds(yearOfBirth, monthOfBirth, dayOfBirth, hourOfBirth, minuteOfBirth, secondOfBirth);
+}
+
+int gigasecond(int currentYear, int currentMonth, int currentDay, int currentHour, int currentMinute, int currentSecond, int yearOfBirth, int monthOfBirth, int dayOfBirth, int hourOfBirth, int minuteOfBirth, int secondOfBirth)
+{
+	return diferenceInSeconds(currentYear, currentMonth, currentDay, currentHour, currentMinute, currentSecond, yearOfBirth, monthOfBirth, dayOfBirth, hourOfBirth, minuteOfBirth, secondOfBirth) > 1000000000 ? 1 : 0 ;
+}
+
+//optimice data types size
+
+
+
+int main()
+{
+	//test
+	std::cout << totalSeconds(2017, 56565656, 0, 0, 0, 0) << std::endl;
+	std::cout << diferenceInSeconds(0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1) << std::endl;
+	std::cout << gigasecond(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1) << std::endl;
+	
+
+
+	std::cin.get();
+}
